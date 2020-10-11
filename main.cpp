a1e5d719b1a82ae25c6a44a4c799fc2b55ee941b
Infromatica II <eder.ariza@udea.edu.co>
2020-10-11 03:13:07 -0500

    Ejercicio 29 - Laboratorio 1

---
+++ main.cpp
@@ -0,0 +1,36 @@
+
+/*
+Ejercicio 29. Escriba un programa que adivine un número A (entre 0 y 100) seleccionado por el
+usuario (el número NO se ingresa al programa), el programa imprimirá en pantalla un número B y
+el usuario usará los símbolos `>', `<' y `=' para indicarle al programa si B es mayor, menor o igual
+que A. El programa imprimira un nuevo número B, con base en simbolo ingresado por el usuario, y
+repetira el proceso hasta acertar el número seleccionado por usuario.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int numero_A = 50;
+    int numero_B;
+
+    while (numero != numero_A){
+
+        cout << "Intente adivinar un numero: ";
+        cin >> numero_B;
+
+        if (numero_B > numero_A)
+            cout << "B > A" << endl;
+
+        else if (numero < numero_aleatorio)
+            cout << "B < A" << endl;
+
+        else
+            cout << "B = A" << endl;
+
+    }
+
+    return 0;
+}
